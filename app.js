c0d0a7a5d6e2fbceb4c35198f49580c7bcefa60d
Errochdi Yassine <yerrochdi.telecomparis@gmail.com>
2023-03-28 17:34:04 +0200

    my first commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var trainsRouter = require('./routes/trains');
 
 var app = express();
 
-// view engine setup
+// view engine setup yes or no
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
